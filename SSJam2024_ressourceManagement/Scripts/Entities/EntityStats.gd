7822e8557bfebe8ad13258595ad9187460e474ec
Akyto <cyrilgajan@gmail.com>
2024-06-10 15:58:21 +0200

    Level path, Ennemy Movement along the path

---
+++ SSJam2024_ressourceManagement/Scripts/Entities/EntityStats.gd
@@ -0,0 +1,8 @@
+extends Node
+
+class_name EntityStats
+@export var AttackRange : float 
+@export var MovementSpeed : float 
+@export var AttackDamage : float
+@export var AttackSpeed :float #attack per second
+@export var Health : float
---
+++ SSJam2024_ressourceManagement/Scripts/EntityMovement.gd
@@ -0,0 +1,8 @@
+extends PathFollow2D
+
+class_name EntityMovement
+
+@export var Stats : EntityStats
+
+func _process(delta):
+	pass
